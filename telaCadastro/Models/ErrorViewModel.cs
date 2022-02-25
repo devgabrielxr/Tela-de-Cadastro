ac2a1a61c66b61e42036229c5a23d37487a3f3e3
Gabriel Xavier Rodrigues <gabrielxavier2.0@hotmail.com>
2022-02-25 20:59:00 -0300

    Add project files.

---
+++ telaCadastro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace telaCadastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
